957ffaf20fa3b46551ee46c364a0282e41922680
aleclister <alec.lister@gmail.com>
2016-06-08 18:22:51 +0200

    changed controllers

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,21 +1,34 @@
 class ToysController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
     @toys = Toy.all
   end
 
   def show
-    @toy = Toy.find(:id)
+    @toy = Toy.find(params[:id])
   end
 
   def create
+    @toy = Toy.new(toy_params)
+    @user = User.find(params[:user_id])
+    redirect_to toys_path
   end
 
   def new
+    @user = User.find(params[:user_id])
+    @toy = Toy.new
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def camera_params
+    params.require(:toy).permit(:brand, :category, :description, :price, :photo)
   end
 end
