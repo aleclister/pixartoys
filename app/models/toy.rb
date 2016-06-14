dcf5cb5147144599db87d751ccda33198728d4f5
aleclister <alec.lister@gmail.com>
2016-06-14 15:52:41 +0200

    indexes on homepage

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,4 +1,6 @@
 class Toy < ActiveRecord::Base
   has_attachment :photo
   has_many :movies
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5], allow_nil: false }
+  validates :rating, numericality: { only_integer: true }
 end
